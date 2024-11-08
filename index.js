57c1a516b36bf15096dd4f7ec2c6948f5f324a1d
Alwin Siby <114428189+alwinsibym@users.noreply.github.com>
2024-11-08 08:16:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-// create needed constants
+// Create needed constants
 const rememberDiv = document.querySelector(".remember");
 const forgetDiv = document.querySelector(".forget");
 const form = document.querySelector("form");
 const nameInput = document.querySelector("#entername");
+const emailInput = document.querySelector("#enteremail");
 const submitBtn = document.querySelector("#submitname");
 const forgetBtn = document.querySelector("#forgetname");
 
-const h1 = document.querySelector("h1");
+const greetingName = document.querySelector("#greeting-name");
 const personalGreeting = document.querySelector(".personal-greeting");
 
-// Stop the form from submitting when the submit button is clicked
+// Stop the form from submitting when a button is pressed
 form.addEventListener("submit", (e) => e.preventDefault());
 
-// Run function when the 'Say hello' button is clicked
+// Run function when the 'Submit' button is clicked
 submitBtn.addEventListener("click", () => {
     const name = nameInput.value.trim();
-    if (name) {
-        // store the entered name in web storage
+    const email = emailInput.value.trim();
+    
+    if (name && email) {
+        // Store the entered name and email in web storage
         localStorage.setItem("name", name);
+        localStorage.setItem("email", email);
         // Update the UI
         nameDisplayCheck();
     }
@@ -25,9 +29,11 @@ submitBtn.addEventListener("click", () => {
 
 // Run function when the 'Forget' button is clicked
 forgetBtn.addEventListener("click", () => {
-    // Remove the stored name from web storage
+    // Remove the stored name and email from web storage
     localStorage.removeItem("name");
-    nameInput.value = "";  // Clear the input field
+    localStorage.removeItem("email");
+    nameInput.value = "";  // Clear the input fields
+    emailInput.value = "";
     // Update the UI
     nameDisplayCheck();
 });
@@ -38,14 +44,14 @@ function nameDisplayCheck() {
 
     if (storedName) {
         // If a name is stored, display personalized greeting
-        h1.textContent = `Welcome, ${storedName}`;
+        greetingName.textContent = storedName;
         personalGreeting.textContent = `Welcome to our website, ${storedName}! We hope you have fun while you are here.`;
         // Hide the 'remember' part of the form and show the 'forget' part
         forgetDiv.style.display = "block";
         rememberDiv.style.display = "none";
     } else {
         // If no name is stored, display generic greeting
-        h1.textContent = "Welcome to our website";
+        greetingName.textContent = "guest";
         personalGreeting.textContent = "Welcome to our website. We hope you have fun while you are here.";
         // Hide the 'forget' part of the form and show the 'remember' part
         forgetDiv.style.display = "none";
