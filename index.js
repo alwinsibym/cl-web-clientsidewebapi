744e8e257596e48c50075d13c897e487fb4da391
Alwin Siby <114428189+alwinsibym@users.noreply.github.com>
2024-11-08 08:48:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Create needed constants
 const rememberDiv = document.querySelector(".remember");
 const forgetDiv = document.querySelector(".forget");
-const form = document.querySelector("form");
+const form = document.querySelector("#contact-form");
 const nameInput = document.querySelector("#entername");
 const emailInput = document.querySelector("#enteremail");
+const phoneInput = document.querySelector("#enterphone");
+const messageInput = document.querySelector("#entermessage");
 const submitBtn = document.querySelector("#submitname");
 const forgetBtn = document.querySelector("#forgetname");
 
-const greetingName = document.querySelector("#greeting-name");
 const personalGreeting = document.querySelector(".personal-greeting");
 
 // Stop the form from submitting when a button is pressed
@@ -15,18 +16,22 @@ form.addEventListener("submit", (e) => e.preventDefault());
 
 // Run function when the 'Submit' button is clicked
 submitBtn.addEventListener("click", () => {
-  // Store the entered name and email in web storage
+  // Store the entered name, email, phone, and message in web storage
   localStorage.setItem("name", nameInput.value);
   localStorage.setItem("email", emailInput.value);
+  localStorage.setItem("phone", phoneInput.value);
+  localStorage.setItem("message", messageInput.value);
   // Update the greeting message
   nameDisplayCheck();
 });
 
 // Run function when the 'Forget' button is clicked
 forgetBtn.addEventListener("click", () => {
-  // Remove the stored name and email from web storage
+  // Remove the stored name, email, phone, and message from web storage
   localStorage.removeItem("name");
   localStorage.removeItem("email");
+  localStorage.removeItem("phone");
+  localStorage.removeItem("message");
   // Update the greeting message
   nameDisplayCheck();
 });
@@ -37,14 +42,12 @@ function nameDisplayCheck() {
   if (localStorage.getItem("name")) {
     // If it is, display personalized greeting
     const name = localStorage.getItem("name");
-    greetingName.textContent = name;
     personalGreeting.textContent = `Welcome to the world of Elon Musk, ${name}! Let’s dive into the visionary mind that’s reshaping humanity’s destiny.`;
     // Hide the 'remember' part of the form and show the 'forget' part
     forgetDiv.style.display = "block";
     rememberDiv.style.display = "none";
   } else {
     // If not, display generic greeting
-    greetingName.textContent = "space enthusiast";
     personalGreeting.textContent =
       "Greetings, space enthusiast! Welcome to the world of Elon Musk, where the future is being written in the stars. Let’s dive into the visionary mind that’s reshaping humanity’s destiny.";
     // Hide the 'forget' part of the form and show the 'remember' part
